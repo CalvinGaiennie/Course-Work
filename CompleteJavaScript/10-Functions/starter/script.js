218f809b3a494f87da3401be87711d20d9accf71
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-15 23:56:42 -0500

    Worked on section 10

--- CompleteJavaScript/10-Functions/starter/script.js
+++ CompleteJavaScript/10-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +25,41 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+*/
+
+/*
+//How Passing Arguments Works
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 2473947284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2473947284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// //Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
+
+////////////////////////////////////////////////////////
+//
