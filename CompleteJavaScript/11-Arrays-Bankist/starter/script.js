30db316f6714eef407ce5102470888f7509f4bf1
CalvinGaiennie <calvingaiennie@gmail.com>
2024-07-30 23:07:24 -0500

    Worked on JS Course

--- CompleteJavaScript/11-Arrays-Bankist/starter/script.js
+++ CompleteJavaScript/11-Arrays-Bankist/starter/script.js
@@ -81,8 +81,20 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-console.log(containerMovements.innerHTMl);
+// console.log(containerMovements.innerHTMl);
 
+const createUsernames = function (acc) {
+  acc.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -230,4 +242,33 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+*/
+/////////////////////////////////////////////////
+//MAP METHOD
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+// how to do this with an arrow function
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
 */
