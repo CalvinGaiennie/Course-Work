b2724b8dbcb81a596b0fc39d866e6429195ecf36
CalvinGaiennie <calvingaiennie@gmail.com>
2024-08-11 22:27:32 -0500

    finished bankist part of section 11

--- CompleteJavaScript/11-Arrays-Bankist/starter/script.js
+++ CompleteJavaScript/11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -202,6 +204,13 @@ btnClose.addEventListener('click', function (e) {
     console.log(accounts);
   }
   inpuutTransferAmount.value = inputTransferTo.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  sorted = !sorted;
+  displayMovements(currentAccount.movements, sorted);
 });
 // console.log(accounts);
 // console.log(accounts);
@@ -572,6 +581,7 @@ const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
 console.log(owners);
 */
+/*
 //Numbers
 console.log(movements);
 
@@ -595,3 +605,4 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 
 console.log(movements);
+*/
