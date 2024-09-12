1158185b46a6b9508acff6c3f031d8b90086855c
CalvinGaiennie <calvingaiennie@gmail.com>
2024-09-11 23:30:39 -0500

    Started OOP in JS

--- CompleteJavaScript/13-Advanced-DOM-Bankist/starter/script.js
+++ CompleteJavaScript/13-Advanced-DOM-Bankist/starter/script.js
@@ -30,15 +30,15 @@ document.addEventListener('keydown', function (e) {
   }
 });
 //////////////////////////////////////////////////////////////
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 const header = document.querySelector('.header');
-console.log(document.querySelectorAll('.section'));
+// console.log(document.querySelectorAll('.section'));
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-console.log(document.getElementsByClassName('btn'));
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
 
 /////////////////////////////
 //creating and inserting elements
@@ -67,46 +67,64 @@ document
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
 
-console.log(message.style.height);
-console.log(message.style.backgroundColor);
-console.log(message.style.color);
-console.log(getComputedStyle(message));
-console.log(getComputedStyle(message).color);
+// console.log(message.style.height);
+// console.log(message.style.backgroundColor);
+// console.log(message.style.color);
+// console.log(getComputedStyle(message));
+// console.log(getComputedStyle(message).color);
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
 
 // access variables in the css root
 // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-//attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// //attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-logo.alt = 'Beatiful minimalist logo';
-console.log(logo.alt);
+// logo.alt = 'Beatiful minimalist logo';
+// console.log(logo.alt);
 
-// Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
+// // Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
 
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
+// // console.log(logo.src);
+// // console.log(logo.getAttribute('src'));
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// // console.log(link.href);
+// // console.log(link.getAttribute('href'));
 
-//Data Attributes
-console.log(logo.dataset.versionNumber);
+// //Data Attributes
+// // console.log(logo.dataset.versionNumber);
 
-//Classes
-logo.classList.add('c', 'j');
-logo.classList.remove('c', 'j');
-logo.classList.toggle('c');
-logo.classList.contains('c'); //not includes
+// //Classes
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'j');
+// logo.classList.toggle('c');
+// logo.classList.contains('c'); //not includes
 
-//dont use
-logo.className = 'jonas';
+// //dont use
+// // logo.className = 'jonas';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current Scroll (X/Y)', window.scrollX, scrollY);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientHeight
+  );
+
+  //Scrolling
+  window.scrollTo(s1coords);
+});
--- CompleteJavaScript/14-OOP/starter/script.js
+++ CompleteJavaScript/14-OOP/starter/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.value = 177;
+
+  //   //Never create a method inside of a constructor function.
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+console.log(matilda);
+
+const jack = new Person('Jack', 1975);
+console.log(jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
+
+//Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+console.log(Person.prototype);
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
