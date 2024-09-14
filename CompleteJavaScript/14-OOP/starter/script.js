46d43f9d0e83b4e8b02fc91a0d24932944184f88
CalvinGaiennie <calvingaiennie@gmail.com>
2024-09-14 02:26:01 -0500

    worked on OOP in JS

--- CompleteJavaScript/14-OOP/starter/script.js
+++ CompleteJavaScript/14-OOP/starter/script.js
@@ -31,6 +31,14 @@ const jay = 'Jay';
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
 
+Person.hey = function () {
+  console.log('Hey there 👋');
+  console.log(this);
+};
+
+Person.hey();
+
+///////////////////////////////////////////////////
 //Prototypes
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
@@ -48,3 +56,131 @@ console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(Person.prototype.isPrototypeOf(Person));
 
 // .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Home Sapiens';
+console.log(jonas.species, matilda.species);
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+console.log(jonas.__proto__.__proto__);
+
+/////////////////////////////////////////////////////////////////
+//Coding Challenge #1
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+/*
+//Make a Car prototype??
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+//Examples
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+console.log(BMW);
+console.log(Mercedes);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+BMW.brake();
+
+console.log(BMW);
+*/
+//////////////////////////////////////////////////////
+//ES6 Classes
+
+//class expression]
+// const PersonCL = class {}
+
+//class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property of the class
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.fullName}`);
+};
+
+jessica.greet();
+
+//1. Classes are NOT hoisted
+//2. Class are first-class citizens
+// you can pass them into and return them from functions
+//3. Classes are executed in strict mode
+
+//////////////////////////////////////////////////////////////
+// Setters and Getters\\
+
+PersonCl.hey();
+const walter = new PersonCl('Walter White', 1965);
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
